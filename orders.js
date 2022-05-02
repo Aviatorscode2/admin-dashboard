9ef9e7d8fd6814d4ae05e6c34a9b65aa4674c71b
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-02 14:59:20 +0100

    Orders.js

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Eba and Eguisi',
+        productNumber: '89632',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Manchester United Away Jersey',
+        productNumber: '41945',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Malt and Bread',
+        productNumber: '23467',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Benz and Range $ASAP',
+        productNumber: '55555',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: '5 Bedroom Suite',
+        productNumber: '90865',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Eba and Eguisi',
+        productNumber: '89632',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
